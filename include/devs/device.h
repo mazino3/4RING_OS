aee0d313112ee6f4d7988b4febd8c41779b0d6ff
isoux <isa@isoux.org>
2021-09-16 19:19:56 +0300

    add file

---
+++ include/devs/device.h
@@ -0,0 +1,27 @@
+/*
+ * 4RING_OS License: MIT
+ *
+ * devs/device.h
+ *
+ * (C) Copyright 2021 Isa <isa@isoux.org>
+ */
+
+
+#ifndef _DEVICE_H
+#define _DEVICE_H
+
+/* devices definitions */
+
+#define NODEV		0
+#define MEM_DEV		1
+#define FD_DEV		2
+#define HD_DEV		3
+#define TTYC_DEV	4
+#define TTY_DEV		5
+#define LP_DEV		1
+#define PIPE_DEV	7
+//#define MEM_DEV	1
+#define BLOK_DEV	10
+#define CHAR_DEV	11
+
+#endif /* _DEVICE_H */
