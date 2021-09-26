a4e34172d4cf235e701ae32ba10c5ff92c5dd8fb
isoux <isa@isoux.org>
2021-09-26 23:22:11 +0300

    Update

--- users/main_task.c
+++ users/main_task.c
@@ -19,22 +19,22 @@ void main_task(void);
 
 int cur_task = 0;
 
-l_long ldt_core[2] =	{
-	0x00c0fa00000007ff,	// Sel. 0x07 TASK_CODE limit=2047 (8Mb) base 0 gran=1
-	0x00c0f200000007ff	// Sel. 0x0f TASK_DATA; 8Mb
+l_long ldt_main[2] =	{
+	0x00c0fa0000000000 + USERS_SYS_LIMIT,	// Sel. 0x07 TASK_CODE limit=2047 (8Mb) base 0 gran=1
+	0x00c0f20000000000 + USERS_SYS_LIMIT,	// Sel. 0x0f TASK_DATA; 8Mb
 };
 
-long stack_tss_users[68];		// 4 * long size more for align
-long stack_tss_users_krn[68];	// Stack for ring 0
-long stack_tss_users_devs[68];	// Stack for ring 1
-long stack_tss_users_libs[68];	// Stack for ring 2
+long stack_tss_main[68];		// 4 * long size more for align
+long stack_tss_main_krn[68];	// Stack for ring 0
+long stack_tss_main_devs[68];	// Stack for ring 1
+long stack_tss_main_libs[68];	// Stack for ring 2
 
-struct stack_ptr p_stack_core = { & stack_tss_users[64]};
-struct stack_ptr p_stack_core_krn = { & stack_tss_users_krn[64]};
-struct stack_ptr p_stack_core_devs = { & stack_tss_users_devs[64]};
-struct stack_ptr p_stack_core_libs = { & stack_tss_users_libs[64]};
+struct stack_ptr p_stack_core = { & stack_tss_main[64]};
+struct stack_ptr p_stack_core_krn = { & stack_tss_main_krn[64]};
+struct stack_ptr p_stack_core_devs = { & stack_tss_main_devs[64]};
+struct stack_ptr p_stack_core_libs = { & stack_tss_main_libs[64]};
 
-struct tss_struct tss_users = {
+struct tss_struct tss_main = {
 	0,				// back link
 	0,				// p_stack_core_krn.end_stack
 	CORE_DATA,		// ss0
@@ -46,9 +46,9 @@ struct tss_struct tss_users = {
 	0,0,0,0,		// eax, ebx, ecx, edx
 	0,				// ptr of p_stack_core.end_stack = esp
 	0,0,0,			// ebp, esi, edi
-	TASK_DATA, TASK_CODE, TASK_DATA, TASK_DATA, TASK_DATA, TASK_DATA,
+	TASK_DATA, TASK_CODE, TASK_DATA, TASK_DATA, TASK_DATA, USRS_ACCES_DATA,
 					// es, cs, ss, ds, fs, gs
-	LDT_MAIN_TASK,	// selector of ldt table
+	LDT_MAIN_TASK,	// selector at GDT of ldt table
 	0x8000000		// trace bitmap
 };
 
@@ -56,13 +56,14 @@ void set_task_stacks(void) {
 
 	long * p0 = (long *) & main_task;
 
-	tss_users.task_stack = p_stack_core.end_stack;
-	tss_users.core_stack = p_stack_core_krn.end_stack;
-	tss_users.ring1_stack = p_stack_core_devs.end_stack;
-	tss_users.ring2_stack = p_stack_core_libs.end_stack;
-	tss_users.task = p0;
+	tss_main.task_stack = p_stack_core.end_stack;
+	tss_main.core_stack = p_stack_core_krn.end_stack;
+	tss_main.ring1_stack = p_stack_core_devs.end_stack;
+	tss_main.ring2_stack = p_stack_core_libs.end_stack;
+	tss_main.task = p0;
 }
 
+/* From here we will enter the first registered user process. */
 __naked void main_task(void) {
   __asm{
 	mov eax, TASK_DATA
@@ -70,5 +71,36 @@ __naked void main_task(void) {
 	l1:
 	jmp l1
   }
+}
+
+__naked void set_usrs_tss_desc(void) {
+__asm{
+	add edi,ecx
+	mov [edi+0x2], ax
+	ror eax, 0x10
+	mov [edi+0x4], al
+	mov [edi+0x7], ah
+	ret
+	}
+}
+
+void set_usrs_desc(long * p_gdt) {
+__asm{
+	mov ecx, [p_gdt]
+
+	lea eax, tss_main
+	mov edi, TSS_MAIN_TASK
+	call set_usrs_tss_desc
+
+	lea eax, ldt_main
+	mov edi, LDT_MAIN_TASK
+	call set_usrs_tss_desc
+	}
+}
+
+void users_init(long * p_gdt) {
+  set_usrs_desc(p_gdt);
+  //set_usrs_tasks();
+  set_task_stacks();
 }
 
