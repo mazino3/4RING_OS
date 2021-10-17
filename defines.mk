80826f0c57837c31bd4fecd1ce087166e11c0d4c
isoux <isa@isoux.org>
2021-10-17 10:37:10 +0300

    Update

--- defines.mk
+++ defines.mk
@@ -14,19 +14,24 @@ CP := cp
 RM := rm -rf
 MKDIR := mkdir -pv
 
+CORE = sys_out/core
+DEVS = sys_out/devs
+LIBS = sys_out/libs
+USRS = sys_out/users
+
+CORE_RAM = core
+DEVS_RAM = devs
+LIBS_RAM = libs
+USRS_RAM = usrs
+GDT_RAM = gdt_
+
 LOAD = sys_out/sys_load.o
 GDT = core/gdt_mem.bin
-GDT_RAM = gdt_
-DEVS_RAM = devs
-CORE_RAM = core
-USRS_RAM = usrs
 SYS = 4ring_os
 SYS_LOAD = load
 IMG_FILES = img/*
 CFG = boot/grub.cfg
-DEVS = sys_out/devs
-CORE = sys_out/core
-USRS = sys_out/users
+
 IMG_PATH := img
 ISO_PATH := iso
 BOOT_PATH := $(ISO_PATH)/boot
