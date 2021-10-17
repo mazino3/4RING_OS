58d33c037ac6d0930aaa2c48a621ce6cf9fb20a1
isoux <isa@isoux.org>
2021-10-17 10:17:08 +0300

    Avoided inline asm code

--- devs/devs_irq.c
+++ devs/devs_irq.c
@@ -1,5 +1,5 @@
 /*
- * 4RING_OS
+ * 4RING_OS License: MIT
  *
  * devs_irq.c
  *
@@ -10,38 +10,39 @@
  */
 
 #include <gdt.h>
+#include <task.h>
 #include <sched.h>
+#include <core/sys_calls.h>
+#include <string.h>
 
-extern int indx;					// table index for sched_req_struc
+extern int indx; /* Table index for sched_req_struc */
 extern struct sched_req_struc dev_sched_tbl[];
+extern struct tss_struct tss_devs_irq;
 
 extern void get_key_int(void);
 
 void devs_irq_task(void);
 
+
 /* Table of interrupt functions at devs ring 1.
  * Here is place to put all main routines of all devices
  * to handle their interrupts.
  */
-void_func dev_int_ftbl[] = {	// devices interrupts functions table
+void_func dev_int_ftbl[] = { /* Devices interrupts functions table */
 0, get_key_int};
 
-static void save_ptr(u_long ptr) {
+static __inlinea_ void save_ptr(u_int ptr) {
   dev_sched_tbl[indx-1].req_ptr = ptr;
 }
 
-__naked void devs_irq_task(void) {
-__asm{
-	start:
-	call [dev_int_ftbl + eax*4]	// sample > call [get_key_int] if eax=1
-	push DECL_IRQ				// input argument, declare IRQ
-	lcall CG_TX_IRQ,0			// call call gate selector sys call
-	mov eax, dword ptr [esp]	// get result from returned call gate func
-	push eax					// push arg on stack
-	call save_ptr				// save returned ptr from core_sched ptrs table
-	add esp,0x04				// release arg from stack
-	iretd						// return from dev irq task
-	jmp short start				// task needs endless loop
-  }
+/* This is main task function for devs IRQ's at ring 1 */
+void devs_irq_task(void) {
+
+  for (;;) { /* This is important for the Task, like a unusual function */
+	  dev_int_ftbl[tss_devs_irq.eax](); // Call function from table index
+	  save_ptr(core_sys_tx_irq()); // Send irq to core and save returned ptr
+	  int_ret(); // Return from interrupt
+  }   /* endless loop for task */
 }
+
 
